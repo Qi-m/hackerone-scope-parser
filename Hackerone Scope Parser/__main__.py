0524783734feb5555422337761aa88ce43539847
Qim How <123775801+Qi-m@users.noreply.github.com>
2025-06-17 10:50:45 -0500

    Update __main__.py
    added wildcard only file option

--- Hackerone Scope Parser/__main__.py
+++ Hackerone Scope Parser/__main__.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 file_name = input("File Name: ")
 finalList = []
@@ -8,9 +9,23 @@ with open(file_name, 'r') as csv_file:
     for l in csv_reader:
         if l[1] in ('WILDCARD', 'URL'):
             finalList.append(l[0])
-finalList.pop(0)
+
+if finalList:
+    finalList.pop(0)
 
 output_file_name = input("Output File Name: ")
+
+wildcard_question = input("Wildcard only file?(y/N): ").lower()
+if wildcard_question == "y":
+    wildcard_file_name = input("Wildcard Only File Name: ")
+    with open(wildcard_file_name, 'w') as wildcards:
+        for i in finalList:
+            match = re.search(r'^\*\.(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}$', i)
+            if match:
+                wildcards.write(i + '\n')
+
+else:
+    print("No Wildcard Only File created!")
 
 with open(output_file_name, 'w') as output:
     for i in finalList:

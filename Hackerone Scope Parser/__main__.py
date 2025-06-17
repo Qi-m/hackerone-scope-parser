a05ccbfa366022ccac411aef5efc58b767b59137
Qim How <123775801+Qi-m@users.noreply.github.com>
2025-06-16 20:47:46 -0500

    Update __main__.py
    added URL and WILDCARD checker and ability to name output file

--- Hackerone Scope Parser/__main__.py
+++ Hackerone Scope Parser/__main__.py
@@ -6,10 +6,12 @@ finalList = []
 with open(file_name, 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
     for l in csv_reader:
-        finalList.append(l[0])
+        if l[1] in ('WILDCARD', 'URL'):
+            finalList.append(l[0])
 finalList.pop(0)
 
-with open('output.txt', 'w') as output:
+output_file_name = input("Output File Name: ")
+
+with open(output_file_name, 'w') as output:
     for i in finalList:
-        output.write(i + '\n')
-        
+        output.write(i + '\n')   
